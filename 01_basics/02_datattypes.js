53386c00c9e382fb78ecc54027ef695ebe7ade76
Vishal Singh Chauhan <106670723+vishalam3636@users.noreply.github.com>
2024-06-03 10:03:56 +0000

    datatypes in js

---
+++ 01_basics/02_datattypes.js
@@ -0,0 +1,35 @@
+"use strict" // treat all js code as newer js
+
+// alert("Hello !!"); // we are using node js and not browser
+
+console.log(
+    
+    3
+    +3); // will work, but wong way, code readibility should be high
+
+
+console.log("vishal");
+
+
+// string => '' or ""
+// number =>  2 to the power 23 or something
+// boolean => true or false
+// bigint => some large number
+// null => standalone value
+// undefined => variable is declared but isn't assigned with any value
+// symbol => to find uniqueness
+
+// object
+
+
+console.log(typeof "Vishal" ,'>>> typeof "Vishal"');
+console.log(typeof 20, ">>> typeof 20");
+console.log(typeof true, ">>>> typeof true");
+console.log(typeof null, ">>>> typeof null");
+console.log(typeof undefined, ">>>>typeof undefined");
+
+console.log(typeof {}, ">>> typeof {}");
+console.log(typeof [], ">>>>> typeof []");
+
+
+
