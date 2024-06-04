43689eefeebcec42cd3ef237fa1af637a1f676d3
Vishal Singh Chauhan <106670723+vishalam3636@users.noreply.github.com>
2024-06-04 10:42:59 +0000

    comparion operators in JS

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+console.log(2>2); // false
+console.log((2 >= 1)); // true
+console.log(2 < 1); // false
+console.log(2 == 1); // false
+console.log(2 != 1); // true
+
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log("2" == 2); // true, does type coercion
+console.log("2" === 2); // false, dont do type coercion
+
+
