73511ea8d2548564e88092c97574930f9e423b47
Vishal Singh Chauhan <106670723+vishalam3636@users.noreply.github.com>
2024-06-07 10:51:49 +0000

    Added the learning of Stack and Heap Memory concept in JS

--- 01_basics/04_ke baad dataTypes-summary.js
+++ 01_basics/04_ke baad dataTypes-summary.js
@@ -52,3 +52,46 @@ checkType(bigNumber); // bigint
 checkType(outsideTemp); // object
 checkType(id); // symbol
 checkType(myFunction); // function
+
+
+
+// +++++++++++++++++++++ STACK AND HEAP MEMORY ++++++++++++++
+/**
+ * Two Type Of Memory:
+ *                  1. Stack (Primitive types ke liye stack memory use hoti hai)
+ *                  2. Heap (Reference types ke liye heap memory use hoti hai)
+ *                  
+ * NOTE:
+ *      Jab bhi "Stack Memory" use hoti hai, hume variable ka copy milta hai
+ *      Jab bhi koi value "Heap Memory" mein jati hai, hume uska reference milta hai, mtlb ki kuch bhi changes krenge to original value mein change hoga
+ * 
+ */
+
+// Example:
+let myName = "Vishal Chauhan"
+let anotherName = myName;
+
+console.log(myName) // Vishal Chauhan
+console.log(anotherName) // Vishal Chauhan
+
+anotherName = "Vishal Singh";
+
+console.log(myName) // Vishal Chauhan
+console.log(anotherName) // Vishal Singh
+
+
+let userOne = {
+    email: "user@yopail.com",
+    user: "user@ybl",
+    city: "Pune"
+}
+
+let userTwo = userOne;
+
+console.log(userOne, ">>> userOne"); // { email: 'user@yopail.com', user: 'user@ybl', city: 'Pune' }
+console.log(userTwo, ">>userTwo"); // { email: 'user@yopail.com', user: 'user@ybl', city: 'Pune' }
+
+userTwo.email = "userrrr@yopmail.com"; // userTwo change krte hi userOne bhi change ho jayega
+
+console.log(userOne, ">>> userOne"); // { email: 'userrrr@yopmail.com', user: 'user@ybl', city: 'Pune' }
+console.log(userTwo, ">>userTwo"); // { email: 'userrrr@yopmail.com', user: 'user@ybl', city: 'Pune' }
