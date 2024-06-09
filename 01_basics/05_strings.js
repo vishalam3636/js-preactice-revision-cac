1541a71b7d15ee46655b78f5a0cd1d7cea1ad58e
Vishal Singh Chauhan <106670723+vishalam3636@users.noreply.github.com>
2024-06-09 15:44:36 +0000

    str topic completed

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,7 +7,7 @@ console.log(name + score + " Score") // outdated way
 console.log(`Hello my name is ${name} and i score ${score} in the game !`);
 
 // another way of declaring string, it creates an Object
-const newName = new String('Vishalsc');
+const newName = new String('Vishal-sc');
 console.log(newName);
 console.log(typeof newName); // object, it has all the string methods, we can check in browsers console
 
@@ -21,16 +21,36 @@ console.log(newName.length); // 8
 
 console.log(newName.toUpperCase()); // VISHALSC
 
+console.log(newName, '>>>> newName') // original value change nahi hui, as its Primitive data type
+
 console.log(newName.charAt(3)); // h (returns character at index 3)
 
+console.log(newName.indexOf("s")); // 2
 
+let newString = newName.substring(0, 4)
+console.log(newString); // Vish
 
+let anotherString = newName.slice(0, 4);
+console.log(anotherString); // Vish
 
+let newStringOne = "   Vishal     ";
+console.log(newStringOne); //     Vishal    
+console.log(newStringOne.trim()); // Vishal (removes spaces)
 
+let url = "https://github.com/vishalam3636/js%20-preactice-revision-cac";
+console.log(url.replace("%20", "")) // https://github.com/vishalam3636/js-preactice-revision-cac
 
+console.log(url.includes("vishal")); // true
+console.log(url.includes("vishalll")); // false
 
+let strArr = newName.split("");
+console.log(strArr); //  ['V','i','s','h','a','l','-','s','c']
 
+strArr = newName.split("-");
+console.log(strArr); // [ 'Vishal', 'sc' ]
 
+strArr = newName.blink();
+console.log(strArr); // <blink>Vishal-sc</blink>
 
 
 
