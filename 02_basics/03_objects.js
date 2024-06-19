95e74fc3b57bae3bb4d0248621c5caf064c2985c
Vishal Singh Chauhan <106670723+vishalam3636@users.noreply.github.com>
2024-06-19 04:35:12 +0000

    Objects in JS part-1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,24 +2,51 @@
 // Literals ki trh declare krne se singleton nahi banta hai, constructor se singleton banta hai.
 // Constructor method, mtlb Object.create() use kr ke 
 
+
+let mySym = Symbol("key1");
+
 let jsUser = {
     name: "Vishal",
     "full name": "Vishal Chauhan",
+    // mySym: "mykey1", // symbol ki trh use nahi ho rha hai, string ki trh use ho rha hai
+    [mySym] : "mykey1",
     age: 18,
     location: "Kanpur",
     email: "visal@google.com",
     isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
+    lastLoginDays: ["Monday", "Saturday"],
 }
 
 // accessing object
 console.log(jsUser.email); // visal@google.com
 console.log(jsUser["email"]); // visal@google.com (accepts an string)
 console.log(jsUser["full name"]); // Vishal Chauhan (is dot se access kr hi nahi skenge)
-                
+
+console.log(jsUser[mySym]); // mykey1, symbol is accessed in sq bkts
+console.log(typeof(jsUser[mySym]));
+
+// changing value
+jsUser["email"] = "vishal@yahoo.in";
+console.log(jsUser);
 
 
+// // freezing object
+// Object.freeze(jsUser);
+// jsUser["email"] = "Madhur@yahoo.in"; // email wont change, as we have freezed the object
+// console.log(jsUser); 
 
+
+// adding function to object
+jsUser.greeting = function(){
+    console.log("Hello JS user !!!");
+}
+jsUser.greeting(); // Hello JS user !!!
+
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name} !!`); // this referest to the parent object
+}
+jsUser.greetingTwo(); // Hello JS user, Vishal !! 
 
 
 
