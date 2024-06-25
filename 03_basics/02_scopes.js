cae667eee92ce9c9017b7a04ed245eb20a805dca
Vishal Singh Chauhan <106670723+vishalam3636@users.noreply.github.com>
2024-06-25 01:55:29 +0000

    Scope level and mini hoisitng in JS over, scope of variable in function anf if-else condition

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -31,3 +31,52 @@ console.log(d); // 40 (problematic, as bahr nahi aana chahiye tha scope ke)
 console.log(a); // 500
 
 
+//++++++++++++++++++++++ Scope Level And Mini Hoisting In JS +++++++++++++++
+// Block level scope
+// Global Scope
+// Nested Scoping
+// how scoping works function inside function
+// Closures
+
+function one(){
+    const username = "Vishal"
+
+    function two(){
+        const website = "netlify";
+        console.log(username); // Vishal
+    }
+    // console.log(website); //ReferenceError: website is not defined
+
+    two();
+}
+one()
+
+
+if(true){
+    const username = "vishal";
+    if(username == "vishal"){
+        const website = " Netlify";
+        console.log(username + website); // vishal Netlify
+    }
+
+    // console.log(website); // ReferenceError: website is not defined
+}
+// console.log(username); // ReferenceError: username is not define
+
+
+//======= interesting (function declaration and function expression, hoisting concept) ======//
+addOne(5)
+console.log(addOne(5)); // 6
+function addOne(num){
+    return num+1;
+}
+// addOne(5)
+
+
+// addTwo(5); // ReferenceError: Cannot access 'addTwo' before initialization
+console.log(addTwo(5)); // ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num+2
+}
+// addTwo(5)
+
